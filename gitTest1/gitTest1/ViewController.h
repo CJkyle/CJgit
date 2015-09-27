8b258dee80e785be296bb0c552a803c97c3dda36
kyleli <chuanjiang_li@163.com>
2015-09-27 17:32:35 +0800

    wrote a gitTest1 file

---
+++ gitTest1/gitTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest1
+//
+//  Created by kyleli on 15/9/27.
+//  Copyright © 2015年 kyleli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest1/gitTest1/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  gitTest1
+//
+//  Created by kyleli on 15/9/27.
+//  Copyright © 2015年 kyleli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
